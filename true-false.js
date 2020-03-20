ad0d5a26166f80ce5d667088159eb005a17c6888
Afia-30 <afiaanjumshampa@gmail.com>
2020-03-20 21:26:34 +0600

    adding done successfully

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//falsyy:
+//false
+//0
+//""
+//undefined
+//null
+//NaN(not a number)
+//Truthy:
+//'0',' ',[],{},
+let name='false';
+if(name){
+    console.log("True");
+
+    }
+    else{
+        console.log("false");
+    }
+}

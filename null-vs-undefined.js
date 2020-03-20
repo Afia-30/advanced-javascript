65de59e6cae28f5ce98d88b8906026b607edb1b1
Afia-30 <afiaanjumshampa@gmail.com>
2020-03-20 21:55:12 +0600

    do add

---
+++ null-vs-undefined.js
@@ -0,0 +1,14 @@
+//let pakhi;
+console.log(pakhi);
+function add(a1,a2){
+    console.log(a1+a2);
+    return
+}
+function add(a1,a2){
+    console.log(a1,a2);
+}
+const premik={name:"smart",phone:0973}
+console.log(premik.gf);
+
+let fun=undefined;
+console.log(fun);
